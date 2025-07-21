a41c88f09289ab45ff3f4e3ba84c7bf5a81c146a
Eux <euclidguillermo28@gmail.com>
2025-07-21 12:54:51 +0800

    Add New IsValidPointer

--- Titanox-C++/libtitanox/MemX/MemX.hpp
+++ Titanox-C++/libtitanox/MemX/MemX.hpp
@@ -23,13 +23,49 @@ namespace MemX {
         }
         return 0;
     }
-
+    /*
+    You Could use this if you want to check if a pointer is valid in a more robust way (recommended for games that doesn't detect mach api calls)
     inline bool IsValidPointer(uintptr_t Address) {
         uint8_t Data = 0;
         size_t Size = 0;
         int KR = vm_read_overwrite(mach_task_self(), (vm_address_t)Address, 1, (vm_address_t)&Data, &Size);
         return !(KR == KERN_INVALID_ADDRESS || KR == KERN_MEMORY_FAILURE || KR == KERN_MEMORY_ERROR);
     }
+    */
+
+    static std::mutex sigsegv_mutex; // Mutex to protect global sigaction from races
+    thread_local sigjmp_buf thread_jump_buffer; // Thread-local jump buffer (isolated per thread)
+    // SIGSEGV handler - long jumps to thread-local buffer
+    void sigsegv_handler(int) { 
+        siglongjmp(thread_jump_buffer, 1);
+    }
+
+    // Checks if an address is in a valid memory region
+    // Uses a signal handler to catch segmentation faults
+    // Returns true if the address is valid, false otherwise
+    inline bool IsValidPointer(uintptr_t address) {
+        std::lock_guard<std::mutex> lock(sigsegv_mutex);
+
+        struct sigaction sa{}, old_sa{};
+        sa.sa_handler = sigsegv_handler;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+
+        if (sigaction(SIGSEGV, &sa, &old_sa) != 0)
+            return false;
+
+        bool result = false;
+
+        if (sigsetjmp(thread_jump_buffer, 1) == 0) {
+            // only checking if access causes a fault
+            *(volatile uintptr_t*)address;
+            result = true;
+        }
+
+        sigaction(SIGSEGV, &old_sa, nullptr);
+        return result;
+    }
+
 
     inline bool _read(uintptr_t addr, void* buffer, size_t len) {
         return IsValidPointer(addr) && (std::memcpy(buffer, reinterpret_cast<void*>(addr), len), true);
